570ed4689670eea634cefea23a9c22a57d0a6b8c
Kayke CS <kaykecsgit@gmail.com>
2024-04-17 22:20:07 -0300

    Database connection and Models

---
+++ Store_MVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store_MVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+    }
+}
